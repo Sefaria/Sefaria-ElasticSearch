5055cf2ba0478e4ec00f3ebc4eeb65a06adfd0a9
Noah Santacruz <noahssantacruz@gmail.com>
2018-04-24 14:08:29 +0300

    updating for elasticsearch 6

--- Sefaria-ElasticSearch-naive-lemmatizer/src/main/java/org/sefaria/sefariaplugin/plugin/SefariaAnalyzerProvider.java
+++ Sefaria-ElasticSearch-naive-lemmatizer/src/main/java/org/sefaria/sefariaplugin/plugin/SefariaAnalyzerProvider.java
@@ -1,34 +1,21 @@
 package org.sefaria.sefariaplugin.plugin;
 
-
-import  org.elasticsearch.common.inject.Inject;
-import  org.elasticsearch.common.inject.assistedinject.Assisted;
-import  org.elasticsearch.common.settings.Settings;
-import  org.elasticsearch.env.Environment;
-import  org.elasticsearch.index.Index;
-import  org.elasticsearch.index.analysis.AbstractIndexAnalyzerProvider;
-import org.elasticsearch.index.settings.IndexSettingsService;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.env.Environment;
+import org.elasticsearch.index.IndexSettings;
+import org.elasticsearch.index.analysis.AbstractIndexAnalyzerProvider;
 import org.sefaria.sefariaplugin.analysis.SefariaAnalyzer;
 
-import  java.io.IOException;
-
 public class SefariaAnalyzerProvider extends AbstractIndexAnalyzerProvider<SefariaAnalyzer> {
+    private final SefariaAnalyzer analyzer;
 
-    /* Constructor. Nothing special here. */
-    @Inject
-    public SefariaAnalyzerProvider(Index index, IndexSettingsService indexSettingsService,
-                                    Environment env, @Assisted String name, @Assisted Settings settings) throws IOException {
-        super(index, indexSettingsService.getSettings(), name, settings);
+    public SefariaAnalyzerProvider(IndexSettings indexSettings, Environment env, String name, Settings settings) {
+        super(indexSettings, name, settings);
+
+        analyzer = new SefariaAnalyzer();
     }
 
-    /* This function needs to be overridden to return an instance of PlusSignAnalyzer. */
-    public SefariaAnalyzer get() {
+    @Override public SefariaAnalyzer get() {
         return this.analyzer;
     }
-
-    /* Instance of PlusSignAnalyzer class that is returned by this class. */
-    protected SefariaAnalyzer analyzer = new SefariaAnalyzer();
-
-    /* Name to associate with this class. We will use this in PlusSignBinderProcessor. */
-    public static final String NAME = "sefaria-naive-lemmatizer";
 }
--- Sefaria-ElasticSearch-naive-lemmatizer/src/main/java/org/sefaria/sefariaplugin/plugin/SefariaPlugin.java
+++ Sefaria-ElasticSearch-naive-lemmatizer/src/main/java/org/sefaria/sefariaplugin/plugin/SefariaPlugin.java
@@ -1,23 +1,29 @@
 package org.sefaria.sefariaplugin.plugin;
-import  org.elasticsearch.index.analysis.AnalysisModule;
+import org.apache.lucene.analysis.Analyzer;
+import org.elasticsearch.index.analysis.AnalyzerProvider;
+import org.elasticsearch.indices.analysis.AnalysisModule;
+import org.elasticsearch.plugins.AnalysisPlugin;
 import  org.elasticsearch.plugins.Plugin;
 
-public class SefariaPlugin extends Plugin {
+import java.util.HashMap;
+import java.util.Map;
 
-    /* Set the name that will be assigned to this plugin. */
+//https://amsterdam.luminis.eu/2017/01/31/creating-elasticsearch-plugin-basics/
+//http://david.pilato.fr/blog/2016/10/19/adding-a-new-rest-endpoint-to-elasticsearch-updated-for-ga/
+//https://github.com/duydo/elasticsearch-analysis-vietnamese/blob/master/src/main/java/org/elasticsearch/plugin/analysis/vi/AnalysisVietnamesePlugin.java
+//https://www.elastic.co/guide/en/elasticsearch/plugins/current/analysis.html
+//GOLD MINE? https://github.com/medcl/elasticsearch-analysis-ik
+//GOLD MINE? https://github.com/medcl/elasticsearch-analysis-pinyin
+//GOLD MINE? https://github.com/ofir123/elasticsearch-network-analysis
+public class SefariaPlugin extends Plugin implements AnalysisPlugin {
+
     @Override
-    public String name() {
-        return "plugin-sefaria-naive-lemmatizer";
+    public Map<String, AnalysisModule.AnalysisProvider<AnalyzerProvider<? extends Analyzer>>> getAnalyzers() {
+        Map<String, AnalysisModule.AnalysisProvider<AnalyzerProvider<? extends Analyzer>>> extra = new HashMap<>();
+
+        extra.put("sefaria-naive-lemmatizer", SefariaAnalyzerProvider::new);
+
+        return extra;
     }
 
-    /* Return a description of this plugin. */
-    @Override
-    public String description() {
-        return "Sefaria!";
-    }
-
-    /* This is the function that will register our analyzer with Elasticsearch. */
-    public void onModule(AnalysisModule analysisModule) {
-        analysisModule.addProcessor(new SefariaBinderProcessor());
-    }
 }
